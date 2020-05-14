d5a47a6169f033476143c95f80c134697c46b69e
scechin <sircechin@yahoo.com>
2020-05-14 20:59:57 +0000

    working on it

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,9 @@
 #Remember import random function here:
-
-my_list = [4,5,734,43,45]
-
+import random
+#random_numbers = []
+random_number = random.sample(range(0, 10), 10)
+#random_numbers += random_number
+my_list = [4,5,734,43,45] + random_number
+#my_list = my_list + random_number
+print(my_list)
 #The magic is here:
