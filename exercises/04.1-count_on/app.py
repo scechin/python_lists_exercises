4e816b5e579eae27031a169a294d0153ac03d15c
scechin <sircechin@yahoo.com>
2020-05-15 15:07:44 +0000

    trying again

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,7 +1,9 @@
 
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
-
 #your code go here:
-
+hello = []
+for item in my_list:
+    if item == type(dict) and item == type(list):
+        hello.append(item)
 print(hello)
